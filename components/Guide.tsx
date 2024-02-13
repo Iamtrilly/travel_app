e0ee257f6c3366bda22df4f2f37862d05fad63ba
Charles Misheto <56826409+Iamtrilly@users.noreply.github.com>
2024-02-13 10:46:20 +0300

    Added GetApp, Footer and made changes to button

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Guide = () => {
   return (
     <section className="flexCenter flex-col">
-      <div className="border-2 padding-container max-container w-full pb-24">
+      <div className="padding-container max-container w-full pb-24">
         <Image src="/camp.svg" alt="camp" width={50} height={50} />
         <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
           We are here for you
@@ -23,7 +23,7 @@ const Guide = () => {
         </div>
       </div>
 
-      <div className="flexCenter max-container relative w-full border-2">
+      <div className="flexCenter max-container relative w-full">
         <Image
           src="/boat.png"
           alt="boat"
